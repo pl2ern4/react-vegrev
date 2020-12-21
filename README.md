af156b7237a82854fb2f861ea72c18c464020c87
pl2ern4 <chaturvediprerna03@gmail.com>
2020-12-21 19:43:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vegrev
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vegrev)
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+import DragAndDrop from "./DragAndDrop";
+
+export default function App() {
+  return <DragAndDrop />;
+}
